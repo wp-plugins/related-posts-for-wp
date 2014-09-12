cd5939de2cb4e171388266bd3b0db991e5867fc9
barrykooij <barrykooij@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-12 20:48:02 +0000

    Release 1.6.0, see readme.txt for changelog.
    git-svn-id: https://plugins.svn.wordpress.org/related-posts-for-wp/tags/1.6.0@988590 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/hooks/class-hook-ajax-install-link-posts.php
+++ classes/hooks/class-hook-ajax-install-link-posts.php
@@ -9,23 +9,31 @@ class RP4WP_Hook_Ajax_Install_Link_Posts extends RP4WP_Hook {
 
 	public function run() {
 
+		// Get the PPR
+		$ppr = isset( $_POST['ppr'] ) ? $_POST['ppr'] : 5;
+
 		// Get the rel amount
-		$rel_amount = isset( $_POST['rel_amount'] ) ? $_POST['rel_amount'] : 5;
+		$rel_amount = isset( $_POST['rel_amount'] ) ? $_POST['rel_amount'] : 3;
 
 		// Related Post Manager object
 		$related_post_manager = new RP4WP_Related_Post_Manager();
 
-		// Link 5 posts
-		if ( true === $related_post_manager->link_related_posts( $rel_amount, 5 ) ) {
+		// Link posts
+		$related_post_manager->link_related_posts( $rel_amount, $ppr );
 
-			// Check if we're done
-			if ( 0 == count( $related_post_manager->get_not_auto_linked_posts( 1 ) ) ) {
-				echo 'done';
-			} else {
-				echo 'more';
-			}
+		// Get uncached post count
+		$uncached_post_count  = $related_post_manager->get_uncached_post_count();
 
+		// Check if we're done
+		if ( $uncached_post_count == 0 ) {
+			// Save the wizard setting as the option
+			$options                                  = RP4WP()->settings->get_options();
+			$options['automatic_linking_post_amount'] = $rel_amount;
+			update_option( 'rp4wp', $options );
 		}
+
+		// Echo the uncached posts
+		echo $uncached_post_count;
 
 		exit;
 	}
